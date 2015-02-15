5defd5ab9fce7f768f57bd29faa49761387febef
Phanuphat Cholatda <remix.tk108@gmail.com>
2015-02-15 14:34:12 +0700

    @HelloWorld : modify class

--- Java/HelloWorld/src/helloworld/HelloWorld.java
+++ Java/HelloWorld/src/helloworld/HelloWorld.java
@@ -1,16 +1,20 @@
-
 package helloworld;
 
 import java.util.Scanner;
 
 public class HelloWorld {
 
+    private HelloWorld() {
+
+    }
 
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Who are you?");
-        System.out.println("Hello "+scanner.nextLine());
+        System.out.println("Hello " + scanner.nextLine());
+
     }
-    
+
 }
